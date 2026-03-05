091e2fb4bc0cd0b4434e447ab438b6cafee53559
João Victor Piacenza de Oliveira Andrade <joaovictorpiacenza@gmail.com>
2026-03-05 10:42:53 -0300

    versao 1.0
    estrutura base do projeto, com o front e back com crud

---
+++ Back-End/server.js
@@ -0,0 +1,53 @@
+const express = require ('express')
+const cors = require('cors')
+const server = express()
+server.use(cors())
+server.use(express.json())
+const pool = require('./db.js')
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+server.listen(PORT,()=>{
+    console.log(`Server rodando no http://localhost:${PORT}/`)
+})
+
+
+server.post("/cadastrar_user",async(req,res)=>{
+    try {
+        const {email,senha} = req.body
+        const [resultado] = await pool.query(`Insert into usuarios (email,senha,nivel) values ("${email}","${senha}","user")`)
+        res.send(resultado)
+    } catch (error) {
+       console.log(error) 
+    }
+})
+
+server.get("/ver_usuarios",async(req,res)=>{
+    try {
+        const [resultado] = await pool.query(`Select * from usuarios`)
+        res.send(resultado)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+server.put("/atualizar_usuarios",async(req,res)=>{
+    try {
+        const {id,email,senha} = req.body
+        const [resultado] = await pool.query(`Update usuarios set email = "${email}", senha = "${senha}" where id = ${id}`)
+        res.send(resultado)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+server.delete("/deletar_usuarios",async(req,res)=>{
+    try {
+        const {id} = req.body
+        const [resultado] = await pool.query(`Delete from usuarios where id = ${id}`)
+        res.send(resultado)
+    } catch (error) {
+        console.log(error)
+    }
+})
---
+++ Front-End/script.js
@@ -0,0 +1,76 @@
+document.getElementById("btn_cadastrar").addEventListener("click",async()=>{
+    const email = document.getElementById("inp_email").value
+    const senha = document.getElementById("inp_senha").value
+
+    const resposta = await fetch(`http://localhost:3000/cadastrar_user`,{
+        method:"post",
+        headers: {
+            "Accept":"application/json",
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            "email":email,
+            "senha":senha
+        })
+    })
+    if(resposta.ok == true){
+        const dados = await resposta.json()
+        alert("Usuário cadastrado")
+    }
+})
+
+document.getElementById("btn_ver").addEventListener("click",async()=>{
+    const resposta = await fetch(`http://localhost:3000/ver_usuarios`,{
+        method:"get",
+        headers: {
+            "Accept":"application/json",
+            "Content-Type":"application/json"
+        }
+    })
+    if(resposta.ok == true){
+        const dados = await resposta.json()
+        document.getElementById("ver").innerText = JSON.stringify(dados)   
+    }
+})
+
+document.getElementById("btn_atualizar").addEventListener("click",async()=>{
+    const id = document.getElementById("upd_id").value
+    const email = document.getElementById("upd_email").value
+    const senha = document.getElementById("upd_senha").value
+
+    const resposta = await fetch(`http://localhost:3000/atualizar_usuarios`,{
+        method:"put",
+        headers: {
+            "Accept":"application/json",
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            "id":id,
+            "email":email,
+            "senha":senha
+        })
+    })
+    if(resposta.ok == true){
+        const dados = await resposta.json()
+        alert("Usuário atualizado")
+    }
+})
+
+document.getElementById("btn_deletar").addEventListener("click",async()=>{
+    const id = document.getElementById("inp_deletar").value
+
+    const resposta = await fetch(`http://localhost:3000/deletar_usuarios`,{
+        method:"delete",
+        headers: {
+            "Accept":"application/json",
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            "id":id
+        })
+    })
+    if(resposta.ok == true){
+        const dados = await resposta.json()
+        alert("Usuário deletado")
+    }
+})
